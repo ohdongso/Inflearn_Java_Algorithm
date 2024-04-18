b07634b28c6b13a2696f8901afa95fbe17ded13a
dongso <ehdth5806@naver.com>
2024-04-18 16:14:35 +0900

    인프런 자바알고리즘 8번째 수업 Commit.

---
+++ src/Section_01_문자열/_07_회문문자열1.java
@@ -0,0 +1,27 @@
+package Section_01_문자열;
+
+import java.util.Scanner;
+
+public class _07_회문문자열1 {
+	
+	public String solution(String str) {
+		String answer = "YES";
+		str = str.toUpperCase();
+		int len = str.length();
+		
+		for(int i = 0; i < len/2; i++) {
+			if(str.charAt(i) != str.charAt(len-i-1)) {
+				return "NO";
+			}
+		}
+		
+		return answer;
+	}
+	
+	public static void main(String[] args) {
+		_07_회문문자열1 T = new _07_회문문자열1();
+		Scanner kb = new Scanner(System.in);
+		String str = kb.next();
+		System.out.print(T.solution(str));		
+	}
+}
