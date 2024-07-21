1407fe76ff20cc314f3ab5529827d22e44483bd3
dongso <ehdth5806@naver.com>
2024-07-21 13:27:09 +0900

    인프런 자바알고리즘 27번째 수업 Commit.

--- src/Section_04_HashMap_TreeSet/_02_아나그램_해쉬.java
+++ src/Section_04_HashMap_TreeSet/_02_아나그램_해쉬.java
@@ -1,9 +1,9 @@
 package Section_04_HashMap_TreeSet;
 
-import java.util.HashMap; 
+import java.util.HashMap;
 import java.util.Scanner;
 
-public class Main {
+public class _02_아나그램_해쉬 {
 	
 	public String solution(String s1, String s2) {
 		String answer = "YES";
@@ -24,7 +24,7 @@ public class Main {
 	}
 	
 	public static void main(String[] args) {
-		Main T = new Main();
+		_02_아나그램_해쉬 T = new _02_아나그램_해쉬();
 		Scanner kb = new Scanner(System.in);
 		String a = kb.next();
 		String b = kb.next();
