fbd30d6b38efd2e61a00b71f5d5f30927544f8f0
dongso <ehdth5806@naver.com>
2024-09-08 15:01:11 +0900

    인프런 자바알고리즘 32번째 수업 Commit

--- src/Section_06_Stack_Queue/_02_괄호문자제거1.java
+++ src/Section_06_Stack_Queue/_02_괄호문자제거1.java
@@ -3,7 +3,7 @@ package Section_06_Stack_Queue;
 import java.util.Scanner;
 import java.util.Stack; 
 
-public class Main {
+public class _02_괄호문자제거1 {
 	/*
 	 	
 	 */
@@ -30,7 +30,7 @@ public class Main {
 	}
 	
 	public static void main(String[] args) {
-		Main T = new Main();
+		_02_괄호문자제거1 T = new _02_괄호문자제거1();
 		Scanner kb = new Scanner(System.in);
 		String str = kb.next();
 		System.out.println(T.solution(str));
